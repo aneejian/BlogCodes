552c3a3a9080a7d861a016c2445e8f02a8c7929b
Aneejian <aneejian@gmail.com>
2017-03-08 22:53:17 +0530

    Add files via upload

---
+++ Angular-JS-Custom-Directive-expand-collapse/controller/DemoController.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Jeena.John on 14-05-2015.
+ */
+
+<!--Creating the module-->
+var demoApp = angular.module('demoApp', []);
+
+<!--Creating the controller-->
+demoApp.controller('DemoController', ['$scope', '$http', function($scope, $http) {
+
+    $http.get('json/customDataSet1.json')
+        .then(function(res){
+            $scope.customDataList = res.data;
+        });
+
+    $http.get('json/customDataSet1.json')
+        .then(function(res){
+            $scope.customDataSet1 = res.data;
+        });
+
+    $http.get('json/customDataSet2.json')
+        .then(function(res){
+            $scope.customDataSet2 = res.data;
+        });
+
+    $scope.dataFlag = false;
+
+    $scope.updateData = function(data, callBackFunction) {
+        if($scope.dataFlag) {
+            var obj = $scope.customDataSet1[data.id - 1];
+            $scope.customDataList[data.id - 1] = cloneObject(obj);
+            $scope.dataFlag = false;
+        } else{
+            var obj = $scope.customDataSet2[data.id - 1];
+            $scope.customDataList[data.id - 1] = cloneObject(obj);
+            $scope.dataFlag = true;
+        }
+
+        callBackFunction();
+    };
+
+    var cloneObject = function (obj) {
+        return JSON.parse(JSON.stringify(obj));
+    }
+}]);
---
+++ Angular-JS-Custom-Directive-expand-collapse/directive/viewMoreDirective.js
@@ -0,0 +1,83 @@
+/**
+ * Created by Jeena John on 12/05/2015.
+ */
+
+'use strict';
+demoApp.directive('viewMore', function ($timeout, $window) {
+    return {
+        restrict: 'E',
+        scope: {
+            customdata: '=',
+            callback: '&'
+        },
+        templateUrl: 'directive/viewMoreDirective.html',
+                    
+        link: function (scope, elm, attrs) {
+
+            var viewMore = function () {
+                var id = scope.customdata.id;
+                var offsetHeight = (elm[0].querySelector('#viewMoreDataDiv' + id)).offsetHeight; //Visible height of the viewMoreDataDiv div
+                var scrollHeight = (elm[0].querySelector('#viewMoreDataDiv' + id)).scrollHeight; //Scrollable height of the viewMoreDataDiv div
+                
+                if ((offsetHeight + 2) < scrollHeight) {
+                    $("#viewMoreDiv" + id).show();
+                } else {
+                    $("#viewMoreDiv" + id).hide();
+                }
+            };
+
+            $timeout(function () {
+                viewMore();
+                scope.$apply();
+            }, 10);
+
+            var w = angular.element($window);
+
+            w.bind('resize', function () {
+                callBackFunction();
+            });
+
+            /** showMore - Called on click of 'View more' link **/
+            scope.showMore = function (id) {
+                var el = $("#viewMoreDataDiv" + id),
+                curHeight = el.height(),
+                autoHeight = el.css('height', 'auto').height(); //Temporarily change to auto and get the height.
+
+                el.height(curHeight).animate({ height: autoHeight }, 600, function () {
+                    /*Now, change the height to auto when animation finishes. 
+                    Added to make the container flexible (Optional).*/
+                    el.css('height', 'auto');
+                });
+                $("#viewMore" + id).hide();
+                $("#viewLess" + id).show();
+            }
+
+            /** showLess - Called on click of 'View more' link **/
+            scope.showLess = function (id) {
+                $("#viewMoreDataDiv" + id).animate({ height: '160px' }, 500);
+                $("#viewMore" + id).show();
+                $("#viewLess" + id).hide();
+            }
+
+            /** updateData - Called on click of refresh button **/
+            scope.updateData = function () {
+                scope.callback({
+                    cbFn: callBackFunction
+                });
+            }
+
+            /** callBackFunction: Used for call back from controller to directive 
+            after the execution of translation function **/
+            var callBackFunction = function () {
+                var id = scope.customdata.id;
+                var $content = $('#viewMoreDataDiv' + id);
+
+                $content.height('160px');
+                $("#viewMore" + id).show();
+                $("#viewLess" + id).hide();
+
+                $timeout(viewMore, 0);
+            }
+        }
+    };
+});
