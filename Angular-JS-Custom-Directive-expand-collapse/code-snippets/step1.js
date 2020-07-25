60bce291888b008f045134f2f0335c5fd52d6da4
Aneejian <aneejian@gmail.com>
2020-07-25 19:19:41 +0530

    Update step1.js

--- Angular-JS-Custom-Directive-expand-collapse/code-snippets/step1.js
+++ Angular-JS-Custom-Directive-expand-collapse/code-snippets/step1.js
@@ -1,5 +1,5 @@
-<!--Creating the module-->
+// Creating the module
 var demoApp = angular.module('demoApp', []);
-<!--Creating the controller-->
+// Creating the controller
 demoApp.controller('DemoController', ['$scope', function($scope) {
 }]);
