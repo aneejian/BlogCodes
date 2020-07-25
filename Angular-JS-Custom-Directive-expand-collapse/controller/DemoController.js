8f1515b66089e2b1d0bfefc419d858f6a3433a9f
Aneejian <aneejian@gmail.com>
2020-07-25 15:38:43 +0530

    Update DemoController.js

--- Angular-JS-Custom-Directive-expand-collapse/controller/DemoController.js
+++ Angular-JS-Custom-Directive-expand-collapse/controller/DemoController.js
@@ -11,10 +11,6 @@ demoApp.controller('DemoController', ['$scope', '$http', function($scope, $http)
     $http.get('json/customDataSet1.json')
         .then(function(res){
             $scope.customDataList = res.data;
-        });
-    
-    $http.get('json/customDataSet1.json')
-        .then(function(res){
             $scope.customDataSet1 = res.data;            
         });
 
